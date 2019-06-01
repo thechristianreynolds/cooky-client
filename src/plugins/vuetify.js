4e4b63a923197a38d8deb5c8524ec1866ef35449
thechristianreynolds <bobsparadox@gmail.com>
2019-05-31 21:33:28 -0600

    Initial commit

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+import 'vuetify/src/stylus/app.styl'
+
+Vue.use(Vuetify, {
+  iconfont: 'md',
+  theme: {
+    primary: '#4CAF50',
+    secondary: '#FF9800',
+    accent: '#C8E6C9',
+  }
+});
